d62bdb2126fa0bf16ec062df0bcf9201debd7bf7
dokilove2@gmail.com <tulmooner2@>
2023-11-06 19:55:46 +0900

    add Scripts

---
+++ Assets/Scripts/Network/ServerSession.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using ServerCore;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace DummyClient
+{
+
+
+   
+
+    class ServerSession : PacketSession
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            PacketManager.Instance.OnRecvPacket(this, buffer);
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            //Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+}
---
+++ Assets/Scripts/Packet/PacketHandler.cs
@@ -0,0 +1,15 @@
+﻿using DummyClient;
+using ServerCore;
+
+class PacketHandler
+{
+    public static void S_ChatHandler(PacketSession session, IPacket packet)
+    {
+        S_Chat chatPacket = packet as S_Chat;
+        ServerSession serverSession = session as ServerSession;
+
+        //if (chatPacket.playerId == 1)
+            //Console.WriteLine(chatPacket.chat);
+        
+    }
+}
